b25e887fe5a5242a4b64cc2403157e0d5a559ea3
grese <grese@yahoo-inc.com>
2015-07-04 22:47:55 -0700

    whoops

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,17 +4,11 @@ import { beforeEach, afterEach } from 'mocha';
 import ModelFactory from 'ember-data-tiny-factory';
 
 setResolver(resolver);
-
+ModelFactory.configure('ember-data-tiny-factory');
 ModelFactory.setResolver(resolver);
-ModelFactory.configure({
-    resolver: resolver,
-    modulePrefix: 'dummy'
-});
-
 beforeEach(function(){
     ModelFactory.setup();
 });
-
 afterEach(function(){
     ModelFactory.teardown();
 });
